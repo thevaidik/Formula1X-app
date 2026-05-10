dcfc47b9118f4cfe137b97b27aba651572b82ea6
Vaidik <vaidik50000@gmail.com>
2026-05-11 02:08:04 +0530

    Initial Commit

---
+++ Formula1X/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Formula1X
+//
+//  Created by Vaidik Dubey on 11/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Formula1X/Formula1XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formula1XApp.swift
+//  Formula1X
+//
+//  Created by Vaidik Dubey on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Formula1XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
